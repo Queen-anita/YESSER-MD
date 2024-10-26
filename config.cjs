c2e6c169931fb7b738ff743e90935ec574457726
Queen-anita <queenanita1122@gmail.com>
2024-10-26 20:35:21 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -20,7 +20,7 @@ const config = {
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
   OWNER_NAME: process.env.OWNER_NAME || "©yessertech",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "255621995482",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "923008340829",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
